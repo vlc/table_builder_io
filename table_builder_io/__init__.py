743bdbb54b113f8f87546f52d61aa4a52d8f9fd8
Matt Richards <mrichards7@outlook.com.au>
2022-04-04 08:59:25 +1000

    add toplevel relative import again

--- table_builder_io/__init__.py
+++ table_builder_io/__init__.py
@@ -1,4 +1,4 @@
 """Tool for reading ABS TableBuilder datasets easily in Python."""
-# from table_builder_io.reader import TableBuilderReader
+from .reader import TableBuilderReader
 
 __version__ = "0.0.1"
